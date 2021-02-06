d2bd872423a001be2ab4d58c0569fcebd02e3522
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-02-06 15:15:34 -0800

    decrypt coded and passes 5 tests"
    exit

--- caesar_cypher/caeser_cypher.py
+++ caesar_cypher/caeser_cypher.py
@@ -6,7 +6,7 @@ import nltk
 
     
 def encrypt(plain, key):
-    """ shifts plaintext input to the letter indicated by key = shift steps """
+    """ shifts plaintext input to the letter indicated by key = shift steps when key is known """
     
     encrypted_text = ''
     # find ascii, add key, % for alpha wrap, back into upper/lower case match input
@@ -29,5 +29,25 @@ def encrypt(plain, key):
 # shift which will restore the encrypted text back to its original form 
 # when correct key is supplied.
 
+def decrypt(encrypted, key):
+    """ shifts encrypted input to the letter indicated by key = shift steps when key is known """
+    
+    decrypted_text = ''
+    # find ascii, subtract previous steps taken (key), % for alpha wrap, 
+    # back into upper/lower case match input
+    for char in encrypted:
+        # lowercase ascii 97-122 inclusive
+        if ord(char) >= 97 and ord(char) <= 122:
+            convert = (ord(char) - key - 97) %26 + 97
+            decrypted_text += chr(convert)
+            
+        # uppercase ascii 65-90 inclusive
+        elif ord(char) >= 65 and ord(char) <= 90:
+            convert = (ord(char) - key - 65) %26 + 65
+            decrypted_text += chr(convert)
+        else:
+            decrypted_text += ''
+
+    return decrypted_text   
         
 # reference on ascii -- https://www.tutorialspoint.com/count-uppercase-lowercase-special-character-and-numeric-values-in-cplusplus#:~:text=Uppercase%20Letters%20%E2%88%92%20A%20%2D%20Z%20having,48%20and%2057%20are%20inclusive
--- tests/test_caesar_cypher.py
+++ tests/test_caesar_cypher.py
@@ -1,29 +1,66 @@
-from caesar_cypher.caeser_cypher import encrypt
+from caesar_cypher.caeser_cypher import encrypt, decrypt
+import pytest
 
-# Passes -- 1. encrypt a string with a given shift
+# Passes all -- 1. encrypt a string with a given shift
 
-# @pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_encrypt_lower():
     actual = encrypt('abc', 10)
     expected = 'klm'
     assert actual == expected
     
-# @pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_encrypt_wrap():
     actual = encrypt('xyz', 10)
     expected = 'hij'
     assert actual == expected
 
-# @pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_encrypt_upper():
     actual = encrypt('JKL', 4)
     expected = 'NOP'
     assert actual == expected
 
-# @pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_encrypt_mixed():
     actual = encrypt('XyZa', 11)
     expected = 'IjKl'
     assert actual == expected
+    
+@pytest.mark.skip("pending")
+def test_encrypt_same():
+    actual = encrypt('abc', 26)
+    expected = 'abc'
+    assert actual == expected
 
-# 2. decrypt a previously encrypted string with the same shift
+# Passes all -- 2. decrypt a previously encrypted string with the same shift
+
+@pytest.mark.skip("pending")
+def test_decrypt_lower():
+    actual = decrypt('klm', 10)
+    expected = 'abc'
+    assert actual == expected
+    
+@pytest.mark.skip("pending")
+def test_decrypt_wrap():
+    actual = decrypt('hij', 10)
+    expected = 'xyz'
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_decrypt_upper():
+    actual = decrypt('NOP', 4)
+    expected = 'JKL'
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_decrypt_mixed():
+    actual = decrypt('IjKl', 11)
+    expected = 'XyZa'
+    assert actual == expected
+
+@pytest.mark.skip("pending")
+def test_decrypt_same():
+    actual = decrypt('abc', 26)
+    expected = 'abc'
+    assert actual == expected
