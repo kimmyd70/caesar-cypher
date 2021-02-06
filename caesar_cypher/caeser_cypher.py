255e48cd0a76c8a5ae53c9bc9a24180c59e17208
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-02-06 15:00:25 -0800

    encrypt function coded and passes 4 tests

---
+++ caesar_cypher/caeser_cypher.py
@@ -0,0 +1,33 @@
+import nltk
+
+# 1. Create an `encrypt` function that takes in a plain text phrase and a numeric shift.
+#     - the phrase will then be shifted that many letters
+#     - shifts that exceed 26 should wrap around
+
+    
+def encrypt(plain, key):
+    """ shifts plaintext input to the letter indicated by key = shift steps """
+    
+    encrypted_text = ''
+    # find ascii, add key, % for alpha wrap, back into upper/lower case match input
+    for char in plain:
+        # lowercase ascii 97-122 inclusive
+        if ord(char) >= 97 and ord(char) <= 122:
+            convert = (ord(char)+ key - 97) %26 + 97
+            encrypted_text += chr(convert)
+            
+        # uppercase ascii 65-90 inclusive
+        elif ord(char) >= 65 and ord(char) <= 90:
+            convert = (ord(char)+ key - 65) %26 + 65
+            encrypted_text += chr(convert)
+        else:
+            encrypted_text += ''
+
+    return encrypted_text   
+    
+# 2. Create a `decrypt` function that takes in encrypted text and numeric 
+# shift which will restore the encrypted text back to its original form 
+# when correct key is supplied.
+
+        
+# reference on ascii -- https://www.tutorialspoint.com/count-uppercase-lowercase-special-character-and-numeric-values-in-cplusplus#:~:text=Uppercase%20Letters%20%E2%88%92%20A%20%2D%20Z%20having,48%20and%2057%20are%20inclusive
--- tests/test_caesar_cypher.py
+++ tests/test_caesar_cypher.py
@@ -1,5 +1,29 @@
-from caesar_cypher import __version__
+from caesar_cypher.caeser_cypher import encrypt
 
+# Passes -- 1. encrypt a string with a given shift
 
-def test_version():
-    assert __version__ == '0.1.0'
+# @pytest.mark.skip("pending")
+def test_encrypt_lower():
+    actual = encrypt('abc', 10)
+    expected = 'klm'
+    assert actual == expected
+    
+# @pytest.mark.skip("pending")
+def test_encrypt_wrap():
+    actual = encrypt('xyz', 10)
+    expected = 'hij'
+    assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_encrypt_upper():
+    actual = encrypt('JKL', 4)
+    expected = 'NOP'
+    assert actual == expected
+
+# @pytest.mark.skip("pending")
+def test_encrypt_mixed():
+    actual = encrypt('XyZa', 11)
+    expected = 'IjKl'
+    assert actual == expected
+
+# 2. decrypt a previously encrypted string with the same shift
